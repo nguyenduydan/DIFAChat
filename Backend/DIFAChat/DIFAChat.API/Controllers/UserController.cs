74aa7331ce1c781533b67fb49b9bf69ba1210c8f
FOG <dn135897@gmail.com>
2025-06-19 15:58:03 +0700

    Add package and test controller

---
+++ Backend/DIFAChat/DIFAChat.API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIFAChat.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController() 
+        { 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ShowList()
+        {
+
+            return Ok("Successed");
+        }
+    }
+}
