edcd4dfe91e437394ae6b2bc5c77426f95a012e2
FOG <dn135897@gmail.com>
2025-06-20 13:38:36 +0700

    Edit again Domain layer

--- Backend/DIFAChat/DIFAChat.Application/Class1.cs
+++ Backend/DIFAChat/DIFAChat.Application/Class1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DIFAChat.Application
+﻿namespace DIFAChat.Application
 {
     public class Class1
     {
+
     }
 }
--- Backend/DIFAChat/DIFAChat.Infrastructure/Class1.cs
+++ Backend/DIFAChat/DIFAChat.Infrastructure/Class1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DIFAChat.Infrastructure
+﻿namespace DIFAChat.Infrastructure
 {
     public class Class1
     {
+
     }
 }
